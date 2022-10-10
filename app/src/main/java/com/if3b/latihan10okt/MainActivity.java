2660a592b750d70f41ec36750383dbd56993c6c4
Adikaraalff <73709213+Adikaraalff@users.noreply.github.com>
2022-10-10 11:38:35 +0700

    update

--- app/src/main/java/com/if3b/latihan10okt/MainActivity.java
+++ app/src/main/java/com/if3b/latihan10okt/MainActivity.java
@@ -13,14 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btnButton =(Button)findViewById(R.id.btn_button);
 
         btnButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "Apa Kabar Kamu", Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
